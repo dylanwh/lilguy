7d9f20f07756c8d1aed99d3f04d119a30db861f5
Dylan Hardison <dylan@hardison.net>
2025-02-15 20:08:25 -0800

    cleanup

--- src/runtime.rs
+++ src/runtime.rs
@@ -246,14 +246,22 @@ impl Runtime {
             package.set("path", parent.join("?.lua").to_string_lossy())?;
         }
 
-        globals.set("sleep", lua.create_async_function(builtin_sleep)?)?;
+
+        globals.set("warn", lua.create_function(builtin_warn)?)?;
+        globals.set("debug", lua.create_function(builtin_debug)?)?;
+        globals.set("info", lua.create_function(builtin_info)?)?;
+
         globals.set("spawn", lua.create_async_function(builtin_spawn)?)?;
+
+        globals.set("sleep", lua.create_async_function(builtin_sleep)?)?;
         globals.set("timeout", lua.create_async_function(builtin_timeout)?)?;
+
         globals.set("markdown", lua.create_function(builtin_markdown)?)?;
 
         let json = lua.create_table()?;
         json.set("encode", lua.create_function(json_encode)?)?;
         json.set("decode", lua.create_function(json_decode)?)?;
+        json.set("null", lua.null())?;
         globals.set("json", json)?;
 
         globals.set("global", Global::new(&services.database))?;
@@ -264,25 +272,7 @@ impl Runtime {
         globals.set("null", lua.null())?;
         globals.set("array_mt", lua.array_metatable())?;
 
-        // lua.set_warning_function(|_, msg, _| {
-        //     tracing::warn!("{msg}");
-        //     Ok(())
-        // });
 
-        // lua already has warn, but let's add debug() and info() as well
-        // should work like print(foo, bar) == print(foo .. bar)
-        globals.set(
-            "debug",
-            lua.create_function(|_, args: LuaMultiValue| {
-                let mut buffer = String::new();
-                for arg in args {
-                    let arg = arg.to_string()?;
-                    buffer.push_str(&arg);
-                }
-                tracing::debug!("{buffer}");
-                Ok(())
-            })?,
-        )?;
 
         globals.set(
             "info",
@@ -315,10 +305,23 @@ impl Runtime {
     }
 }
 
-fn json_encode(_: &Lua, value: LuaValue) -> LuaResult<String> {
+/// json.encode(value, options)
+/// where options is an optional table with a single key `pretty`
+/// if `pretty` is true, the output will be pretty printed (indented)
+fn json_encode(_: &Lua, (value, options): (LuaValue, Option<LuaTable>)) -> LuaResult<String> {
+    let pretty = options
+        .and_then(|options| options.get("pretty").ok())
+        .unwrap_or(false);
+    if pretty {
+        return serde_json::to_string_pretty(&value).map_err(LuaError::external);
+    }
     serde_json::to_string(&value).map_err(LuaError::external)
 }
 
+
+/// json.decode(value)
+/// where value is a string containing json
+/// returns a lua value
 fn json_decode(lua: &Lua, value: String) -> LuaResult<LuaValue> {
     let value: serde_json::Value = serde_json::from_str(&value).map_err(LuaError::external)?;
     lua.to_value(&value)
@@ -356,4 +359,34 @@ fn builtin_markdown(_lua: &Lua, value: String) -> LuaResult<String> {
         &value,
         &comrak::ComrakOptions::default(),
     ))
+}
+
+fn builtin_warn(_lua: &Lua, args: LuaMultiValue) -> LuaResult<()> {
+    let mut buffer = String::new();
+    for arg in args {
+        let arg = arg.to_string()?;
+        buffer.push_str(&arg);
+    }
+    tracing::warn!("{buffer}");
+    Ok(())
+}
+
+fn builtin_debug(_lua: &Lua, args: LuaMultiValue) -> LuaResult<()> {
+    let mut buffer = String::new();
+    for arg in args {
+        let arg = arg.to_string()?;
+        buffer.push_str(&arg);
+    }
+    tracing::debug!("{buffer}");
+    Ok(())
+}
+
+fn builtin_info(_lua: &Lua, args: LuaMultiValue) -> LuaResult<()> {
+    let mut buffer = String::new();
+    for arg in args {
+        let arg = arg.to_string()?;
+        buffer.push_str(&arg);
+    }
+    tracing::info!("{buffer}");
+    Ok(())
 }
